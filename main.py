f1d02133432fca19071ae77ef29df16c9533d092
Zebco <zebco1382@gmail.com>
2022-11-29 14:25:55 +0330

    big update

--- main.py
+++ main.py
@@ -5,33 +5,32 @@ import threading
 import schedule
 import json
 
-DB_ADDRESS = 'x-ui.db'
+DB_ADDRESS = '/etc/x-ui/x-ui.db'
 USER_LAST_ID = 0
 LIMIT_ACCOUNT_TRAFFIC = 5 * 1024 * 1024 * 1024 # GB
 
 
-def bytes_to_GB(bytes):
-    return bytes / 1024 / 1024 / 1024
-
 def getUsers() -> list[dict]:
     global USER_LAST_ID
     conn = sqlite3.connect(DB_ADDRESS)
-    cursor = conn.execute(f"select id,remark,port,settings,protocol from inbounds where id > {USER_LAST_ID}")
+    cursor = conn.execute(f"select id,remark,port,settings,protocol,total from inbounds where id > {USER_LAST_ID}")
     users_list = []
     for c in cursor:
-        users_list.append({'name':c[1],'port':c[2],'users':json.loads(c[3]), "protocol": c[4]})
+        users_list.append({"name":c[1],"port":c[2],"users":json.loads(c[3]), "protocol": c[4], "total": c[5]})
         USER_LAST_ID = c[0]
     conn.close()
     return users_list
 
-def limitAccount(user_port):
+def limitAccount(user_port, total):
     conn = sqlite3.connect(DB_ADDRESS)
-    conn.execute(f"update inbounds set total = {LIMIT_ACCOUNT_TRAFFIC} where port={user_port}")
+    if total <= LIMIT_ACCOUNT_TRAFFIC:
+        conn.execute('update inbounds set enable = 0 where port={user_port}')
+        print(f'port {user_port} disabled')
+    else:
+        conn.execute(f"update inbounds set total = {LIMIT_ACCOUNT_TRAFFIC} where port={user_port}")
+        print(f'port {user_port} limited')
     conn.commit()
     conn.close()
-    time.sleep(2)
-    os.popen("x-ui restart")
-    time.sleep(3)
     
 def checkNewUsers():
     # conn = sqlite3.connect(_db_address)
@@ -44,11 +43,15 @@ def checkNewUsers():
 
 def init():
     users_list = getUsers()
+    approved_num = 0
     for user in users_list:
         if not len(user['users']['clients']) > 0:
             continue
-        max_conn = user['users']['clients'][0]['email']
-        if max_conn == 'unlimited':
+        try:
+            max_conn = user['users']['clients'][0]['email']
+        except KeyError:
+            continue
+        if max_conn in ['unlimited', '0']:
             continue
         try:
             max_conn = int(max_conn)
@@ -57,6 +60,9 @@ def init():
         thread = AccessChecker(user, max_conn)
         thread.start()
         print("starting checker for : " + user['name'])
+        approved_num += 1
+    if approved_num > 0:
+        os.popen("x-ui restart")
 
 class AccessChecker(threading.Thread):
     def __init__(self, user, max_conn):
@@ -64,24 +70,32 @@ class AccessChecker(threading.Thread):
         self.user = user
         self.max_conn = max_conn
 
+    def stop(self):
+        self._stop.set()
+
     def run(self):
         user_protocol = self.user['protocol']
         if str(user_protocol).lower() not in ['vmess', 'vless', 'shadowsocks', 'trojan']:
             return
         user_remark = self.user['name']
         user_port = self.user['port']
+        user_total_traffic = self.user['total']
         while True:
-            netstate_data =  os.popen("netstat -np 2>/dev/null | grep :"+str(user_port)+" | awk '{if($3!=0) print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head").read()
-            netstate_data = str(netstate_data)
-            connection_count =  len(netstate_data.split("\n")) - 1
+            netstate_data =  os.popen("netstat -np 2> /dev/null | grep :'%s' | awk '{print $5}' | cut -d ':' -f 1 | sort -u | paste -s -d, -" % str(user_port)).read()
+            ips_connected = netstate_data.split(",")
+            if "\n" == ips_connected[0]:
+                del ips_connected[0]
+            connection_count = len(ips_connected)
+            print(connection_count)
             if connection_count > self.max_conn:
                 user_remark = user_remark.replace(" ","%20")
-                limitAccount(user_port=user_port)
-                print(f"inbound with port {user_port} blocked")
+                limitAccount(user_port=user_port, total=user_total_traffic)
+                self.stop()
             else:
                 time.sleep(2)
 
 init()
+print(f'Account Limit Conection Started!')
 schedule.every(1).minutes.do(checkNewUsers)
 while True:
     schedule.run_pending()
